39cd4bad0198f8de220dab12f9767ad6c2b6238e
egidiocs <ecs@bloco.it>
2019-10-21 16:41:14 -0300

    widgets-text

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,30 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var textStyle = TextStyle(
+      fontSize: 50,
+      fontWeight: FontWeight.bold,
+    );
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My Cool App'),
+          title: Text("myApp"),
+          backgroundColor: Colors.amber,
         ),
         body: Center(
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                //shape: BoxShape.circle,
-                //border: Border.all(width: 4, color: Colors.black),
-                //boxShadow: [
-                //  BoxShadow(offset: Offset(40, 40), color: Colors.purple),
-                //  BoxShadow(offset: Offset(20, 20), color: Colors.pink),
-                //],
-                gradient: RadialGradient(colors: [Colors.yellow, Colors.pink])),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  height: 100,
+                  child: Text(
+                    'Hello World this is too long',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.display3,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
