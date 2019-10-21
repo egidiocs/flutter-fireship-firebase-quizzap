5b88a97a918e180431af6772565420de0ef08ad3
egidiocs <ecs@bloco.it>
2019-10-21 20:41:45 -0300

    widgets-animated

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ScrollController _controller = ScrollController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("myApp"),
-          backgroundColor: Colors.amber,
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amberAccent,
-          child: Icon(Icons.add_box),
-          onPressed: () => _controller.animateTo(_controller.offset + 10000,
-              curve: Curves.easeIn, duration: Duration(milliseconds: 500)),
-        ),
-        body: ListView.builder(
-          controller: _controller,
-          itemBuilder: (context, idx) {
-            return Container(
-              color: Colors.blue,
-              margin: EdgeInsets.all(10),
-              height: 100,
-              child: Center(
-                child: Text(
-                  '$idx',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+          body: Center(
+        child: CoolBox(),
+      )),
     );
   }
 }
 
-List<Widget> _cards() {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      .map((v) => Container(
-            color: Colors.blue,
-            margin: EdgeInsets.all(1),
-            child: Center(
-              child: Text(
-                '$v',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ))
-      .toList();
+class CoolBox extends StatefulWidget {
+  const CoolBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CoolBoxState createState() => _CoolBoxState();
+}
+
+class _CoolBoxState extends State<CoolBox> {
+  double width = 100;
+  double height = 100;
+  Color color = Colors.green;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+        duration: Duration(seconds: 2),
+        curve: Curves.bounceInOut,
+        color: color,
+        width: width,
+        height: height,
+        child: FlatButton(
+          child: Text('Random'),
+          onPressed: () {
+            setState(() {
+              width = Random().nextDouble() * 400;
+              height = Random().nextDouble() * 400;
+
+              int r = Random().nextInt(255);
+              int b = Random().nextInt(255);
+              int g = Random().nextInt(255);
+              color = Color.fromRGBO(r, b, g, 1);
+            });
+          },
+        ));
+  }
 }
