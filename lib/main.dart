38613aa431de3cf09f88343ce23ba603eae2d6bb
egidiocs <ecs@bloco.it>
2019-10-22 17:12:30 -0300

    concepts-async StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,11 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text('Home')),
         body: Center(
-          child: FutureBuilder<DocumentSnapshot>(
-              future: db
+          child: StreamBuilder<DocumentSnapshot>(
+              stream: db
                   .collection('users')
                   .document('mB6sGaFBczfIW50DJyvGDcQWOvW2')
-                  .get(),
+                  .snapshots(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   var data = snapshot.data.data;
