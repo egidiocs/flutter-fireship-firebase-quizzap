792ff3932baa77436fb21af495a65675e3fe8ebd
egidiocs <ecs@bloco.it>
2019-10-22 16:13:21 -0300

    concepts-routing

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.black,
-          textTheme: TextTheme(
-              body1: TextStyle(color: Colors.red.shade100, fontSize: 30),
-              headline: TextStyle(color: Colors.blue.shade100, fontSize: 70))),
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/slideshow': (context) => SlideshowScreen()
+      },
       home: HomeScreen(),
     );
   }
@@ -21,16 +19,90 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            iconTheme: IconThemeData(color: Colors.white, size: 30),
-            title: Row(
-              children: <Widget>[Icon(Icons.apps), Text('MyApp')],
-            )),
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: Theme.of(context).textTheme.body1,
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('push'),
+              color: Colors.green,
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
+                // );
+
+                //  Navigator.pushNamed(
+                //     context,
+                //     '/slideshow'
+                //   );
+
+                Navigator.pushNamed(context, '/slideshow');
+              },
+            ),
+            FlatButton(
+              child: Text('push replacement'),
+              color: Colors.blue,
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
+                // );
+
+                //  Navigator.pushNamed(
+                //     context,
+                //     '/slideshow'
+                //   );
+
+                Navigator.pushReplacementNamed(context, '/slideshow');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SlideshowScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SlideshowScreen'),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('home'),
+              color: Colors.green,
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SlideshowScreen(name: 'Jeff'))
+                // );
+
+                //  Navigator.pushNamed(
+                //     context,
+                //     '/slideshow'
+                //   );
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                } else {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
