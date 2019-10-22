9ccca583d472556eec40ef96fd866ab0803ee1d4
egidiocs <ecs@bloco.it>
2019-10-22 14:27:28 -0300

    concepts-themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -7,53 +6,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: Center(
-        child: CoolBox(),
-      )),
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.lightGreen,
+          textTheme: TextTheme(
+              body1: TextStyle(color: Colors.red, fontSize: 30),
+              headline: TextStyle(color: Colors.blue, fontSize: 70))),
+      home: HomeScreen(),
     );
   }
 }
 
-class CoolBox extends StatefulWidget {
-  const CoolBox({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _CoolBoxState createState() => _CoolBoxState();
-}
-
-class _CoolBoxState extends State<CoolBox> {
-  double width = 100;
-  double height = 100;
-  Color color = Colors.green;
-  double a = 1;
-  String text;
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-        duration: Duration(seconds: 1),
-        curve: Curves.linear,
-        color: color,
-        width: width,
-        height: height,
-        child: FlatButton(
-          child: Text('Random $text'),
-          onPressed: () {
-            setState(() {
-              width = Random().nextDouble() * 400;
-              height = Random().nextDouble() * 400;
-
-              int r = Random().nextInt(255);
-              int b = Random().nextInt(255);
-              int g = Random().nextInt(255);
-              a = Random().nextDouble();
-
-              text = a.toString();
-              color = Color.fromRGBO(r, b, g, a);
-            });
-          },
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: Theme.of(context).textTheme.headline,
+          ),
         ));
   }
 }
