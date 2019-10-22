1466862c42b4ea3a53b1de9f18f8ce5ada4d2db7
egidiocs <ecs@bloco.it>
2019-10-22 17:49:53 -0300

    concepts-state

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:async';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
+class UserModel {
+ String name = 'Bob';
+}
+
+var stream = Stream.fromIterable([UserModel()]);
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return StreamProvider<UserModel>.value(
+     value: stream,
+     child: MaterialApp(
+       home: HomeScreen(),
+     ),
+   );
+ }
 }
 
 class HomeScreen extends StatelessWidget {
-  final Firestore db = Firestore.instance;
+ final Firestore db = Firestore.instance;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Home')),
-        body: Center(
-          child: StreamBuilder<DocumentSnapshot>(
-              stream: db
-                  .collection('users')
-                  .document('mB6sGaFBczfIW50DJyvGDcQWOvW2')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  var data = snapshot.data.data;
+ @override
+ Widget build(BuildContext context) {
 
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.network(data['photoURL']),
-                      Text(
-                        data['username'],
-                        style: Theme.of(context).textTheme.display1,
-                      ),
-                    ],
-                  );
-                } else {
-                  return Container();
-                }
-              }),
-        ));
-  }
+   var user = Provider.of<UserModel>(context);
+
+   return Scaffold(
+     body: Center(
+       child: Text(user.name),
+     ),
+   );
+ }
 }
